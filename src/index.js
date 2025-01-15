ac639d2c585901f9bd0e404c9490813460268bfc
qiuxan <qiuxan@gmail.com>
2025-01-15 17:31:16 +1100

    Add the basic btn and input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('Hello, world!');
+const root = document.getElementById('root');
+const addBook = document.getElementById('addBook');
+const delBook = document.getElementById('delBook');
+const bookList = document.getElementById('bookList');
+
+const addBookBtn = document.createElement('button');
+const bookNameInput = document.createElement('input');
+const delBookBtn = document.createElement('button');
+const bookIdInput = document.createElement('input');
+
+addBookBtn.textContent = 'Add Book';
+delBookBtn.textContent = 'Delete Book';
+
+addBook.addEventListener('click', addBookHandler);
+delBook.addEventListener('click', delBookHandler);
+
+function addBookHandler() {
+  console.log('Add Book');
+}
+
+function delBookHandler() {
+   console.log('Delete Book');
+}
+
+addBook.appendChild(bookNameInput);
+addBook.appendChild(addBookBtn);
+
+delBook.appendChild(bookIdInput);
+delBook.appendChild(delBookBtn);
