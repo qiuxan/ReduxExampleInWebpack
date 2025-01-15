d8e584b08fa06d09cd1a606f3106eb6327005706
qiuxan <qiuxan@gmail.com>
2025-01-15 19:33:17 +1100

    Demo Redux

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+import { configureStore, current } from '@reduxjs/toolkit';
+
+let recordState;
+const  initialState = [];
+
+const rootReducer = (state = initialState, action) => {
+
+  recordState = state;
+  switch(action.type) {
+    case 'ADD_BOOK':
+      return [...state, 
+        {
+          bookId: action.payload.bookId,
+          bookName: `< ${action.payload.bookName}>`,
+        }
+
+      ];
+      
+    case 'DEL_BOOK':
+      return state.filter((book) => book.bookId != action.payload.bookId);
+    default:
+      return [...state];
+  }
+}
+
+//create store
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+
 const root = document.getElementById('root');
 const addBook = document.getElementById('addBook');
 const delBook = document.getElementById('delBook');
@@ -14,16 +45,76 @@ delBookBtn.textContent = 'Delete Book';
 addBook.addEventListener('click', addBookHandler);
 delBook.addEventListener('click', delBookHandler);
 
+function* idGenerator() {
+  let id = 0;
+  while(true) {
+    yield id++;
+  }
+}
+const generateId = idGenerator();
+const getNewBookId = ()=>generateId.next().value;
+
+
 function addBookHandler() {
-  console.log('Add Book');
+
+  const bookName = bookNameInput.value;
+
+  if(bookName) {
+    const  bookId = getNewBookId();
+    bookNameInput.value = '';
+    const action = {
+      type: 'ADD_BOOK',
+      payload: {
+        bookId,
+        bookName,
+      }
+
+    }
+
+    store.dispatch(action);
+
+  }
+
 }
 
 function delBookHandler() {
-   console.log('Delete Book');
+  const bookId = bookIdInput.value;
+  if(bookId) {
+    bookIdInput.value = '';
+    const action = {
+      type: 'DEL_BOOK',
+      payload: {
+        bookId,
+      }
+    }
+    store.dispatch(action);
+  } 
 }
-
 addBook.appendChild(bookNameInput);
 addBook.appendChild(addBookBtn);
 
 delBook.appendChild(bookIdInput);
 delBook.appendChild(delBookBtn);
+
+const showState = store.subscribe(() => {
+  const state = store.getState();
+  console.log({state});
+});
+
+const showBookList = store.subscribe(() => {
+  const currentState = store.getState();
+
+  if(currentState.length != recordState.length) {
+    bookList.innerText = '';
+    currentState.forEach((book) => {
+      createBooklist(book);
+    });
+  }
+
+});
+
+function createBooklist(info){
+  const bookItem = document.createElement('li');
+  bookItem.innerText = `bookId: ${info.bookId} bookName: ${info.bookName}`;
+  bookList.appendChild(bookItem);
+}
